92de2882b2c4eb0e004f0759a52beb2cdbf7916c
Muhad99 <145128166+Muhad99@users.noreply.github.com>
2024-11-13 12:23:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=1bd4ZukeMLA")
+})
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=1bd4ZukeMLA")
+})
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=1bd4ZukeMLA")
+})
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://jascubetechnologies.wordpress.com/2021/11/28/new-office/")
+})
+document.getElementsByClassName("article")[1].addEventListener("click", function(){
+    window.open("https://jascubetechnologies.wordpress.com/2021/11/28/new-office/")
+})
+document.getElementsByClassName("article")[2].addEventListener("click", function(){
+    window.open("https://jascubetechnologies.wordpress.com/2021/11/28/new-office/")
+})
